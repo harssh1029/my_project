472e2e7fa2fdf4663e9b73c7ec6f9ba5bd3496a5
harssh1029 <harshupadhyay0429@gmail.com>
2025-01-20 01:31:31 +0530

    dashboard socket url updated setting file

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zyr#n90^&a5d5j65&g9z7egv0ijyb2yuyfzop*pg=+@!sf+osu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['*']
 
 
 # Application definition
